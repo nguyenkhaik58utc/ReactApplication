ec150a9afd63182044a5e1c71ce286ff33da6a72
khai nguyen <khainv6@fpt.com.vn>
2022-06-01 20:33:34 +0700

    update demo.js

--- src/Components/demo.js
+++ src/Components/demo.js
@@ -9,21 +9,25 @@ export const Demo = () => {
   const dispatch = useDispatch()
   console.log("isLoading",isLoading);
   //var a = props.FirstValue;
-  var b = getList().then( (result) => {
-    console.log(result);
-  }) ;
-  //const [b,setValB] = useState([]);
+  //getList().then( (result) => {
+    //setValB(result)
+  //}) ;
+  const [b,setValB] = useState([]);
   //const [valInput,setValInput] = useState("");
   // const handInput = (e) => {
   //     //a = e.target.value;
   //     setValInput (e.target.value)
   // }
-//   useEffect(() => {
-//       if(b.length <= 0 ){
-//           setValB(getList());
-//       }
+   useEffect(() => {
+       if(b.length <= 0 ){
+            getList().then( (result) => {
+              setValB(result)
+            }) 
 
-//   }, [])
+       }
+
+   }, [])
+
 
 const handButton = (e) => {
   if(isLoading){
@@ -39,6 +43,30 @@ const handButton = (e) => {
       <button onClick={(e) => handButton(e)}>Toggle</button>
       <div>a:</div>
       <div>b: {isLoading ? "loading" : "no loading"}</div>
+      <div> 
+        <table>
+          <thead>
+            <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Image</td>
+            <td>CategoryId</td>
+            <td>Price</td>
+            </tr>
+          </thead>
+          <tbody>
+          {b.map((el) =>
+              <tr key={el.id}>
+                <td>{el.id}</td>
+                <td>{el.name}</td>
+                <td>{el.Images}</td>
+                <td>{el.CategoryId}</td>
+                <td>{el.Price}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
       {/* <div>b: {b[0].name}</div> */}
     </React.Fragment>
   );
