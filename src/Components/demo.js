33c79b7535b6f1a6ab42b8c0b8aab2b8be112892
khai nguyen <nguyenkhaik58utc@gmail.com>
2022-06-01 20:34:45 +0700

    update

--- src/Components/demo.js
+++ src/Components/demo.js
@@ -6,24 +6,23 @@ import {ProductAction} from "./../Stores/IndexAction"
 export const Demo = () => {
 
   const isLoading  = useSelector((state) => state.product.IsLoading);
-  const dispatch = useDispatch()
-  console.log("isLoading",isLoading);
+  const dispatch = useDispatch();
   //var a = props.FirstValue;
   var b = getList().then( (result) => {
     console.log(result);
   }) ;
-  //const [b,setValB] = useState([]);
+  const [b,setValB] = useState([]);
   //const [valInput,setValInput] = useState("");
   // const handInput = (e) => {
   //     //a = e.target.value;
   //     setValInput (e.target.value)
   // }
-//   useEffect(() => {
-//       if(b.length <= 0 ){
-//           setValB(getList());
-//       }
+  useEffect(() => {
+       if(b.length <= 0 ){
+           setValB(getList());
+       }
 
-//   }, [])
+   }, [])
 
 const handButton = (e) => {
   if(isLoading){
@@ -37,7 +36,7 @@ const handButton = (e) => {
   return (
     <React.Fragment>
       <button onClick={(e) => handButton(e)}>Toggle</button>
-      <div>a:</div>
+      <div>a: {b[0].name}</div>
       <div>b: {isLoading ? "loading" : "no loading"}</div>
       {/* <div>b: {b[0].name}</div> */}
     </React.Fragment>
